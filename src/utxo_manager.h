205a7bbe79136f3370b80858c680ff5510427b9a
Yagami-light45 <harshithpasupuleti11@gmail.com>
2026-01-29 20:48:00 +0530

    new feature : utxo-manager

---
+++ src/utxo_manager.h
@@ -0,0 +1,38 @@
+#ifndef UTXO_MANAGER_H
+#define UTXO_MANAGER_H
+
+#include "utils.h"
+
+class UTXO {
+    private:
+        string tx_id;
+        string owner_id;
+        double amount;
+        int index;
+    public:
+        UTXO();
+        UTXO(string tx_id, string owner_id, double amount, int index);
+        
+        string getOwnerId() const;
+        double getAmountInUTXO() const;
+        void printData();
+};
+
+class UTXO_manager {
+    private:
+        // Maps (Transaction ID, Index)
+        map<pair<string,int>, UTXO> UTXO_set;
+    public: 
+        bool add_utxo(string tx_id, string owner_id, double amount, int index);
+        bool remove_utxo(string tx_id, int index);
+        
+        double get_balance(string user_id) const;
+        bool is_unspent(string tx_id, int index) const;
+        
+        vector<UTXO> getUTXOs(string user_id);
+        bool isBelongsTo(string tx_id, int index, string owner_id);
+        double getUTXOValue(string tx_id, int index);
+        vector<UTXO> getAllUTXOs();
+};
+
+#endif
